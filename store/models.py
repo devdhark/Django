3868621bd468acc1c543cda08d4a845c48fc12d0
devdhark <devdhar.kodiatar@gmail.com>
2023-07-26 15:07:42 -0500

    add promotions for products as a many to many relationship

--- store/models.py
+++ store/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
 
 
 class Product(models.Model):
@@ -12,6 +20,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
@@ -46,9 +55,9 @@ class Order(models.Model):
     PAYMENT_STATUS_FAILED = "F"
 
     PAYMENT_STATUS_CHOICES = [
-        (PAYMENT_STATUS_PENDING, "Pending")(PAYMENT_STATUS_COMPLETE, "Complete")(
-            PAYMENT_STATUS_FAILED, "Failed"
-        )
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
