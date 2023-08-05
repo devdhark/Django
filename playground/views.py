26ab32f9191a4f5c6a0288e7e0e100313fb97211
devdhark <devdhar.kodiatar@gmail.com>
2023-08-04 23:31:47 -0500

    update

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,4 @@ from store.models import Product
 def say_hello(request):
     query_set = Product.objects.all()
 
-    for product in query_set:
-        print(product)
-
     return render(request, "hello.html", {"name": "Devdhar"})
