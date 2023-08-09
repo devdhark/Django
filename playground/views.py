954e44a6c8c707bea3b3d635a620262e953f1105
devdhark <devdhar.kodiatar@gmail.com>
2023-08-08 21:40:55 -0500

    pass keyword arguments to Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Collection, Customer, OrderItem, Product
 
 
 def say_hello(request):
-    queryset = OrderItem.objects.filter(product__collection__id=True)
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
 
     return render(
-        request, "hello.html", {"name": "Devdhar", "orderitems": list(queryset)}
+        request, "hello.html", {"name": "Devdhar", "products": list(queryset)}
     )
