133159e88603141993a24d3c62167fbd02a23d3e
devdhark <devdhar.kodiatar@gmail.com>
2023-08-17 16:34:43 -0500

    concat class

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value
+from django.db.models import Q, F, Value, Func
 from django.db.models.aggregates import Count, Max, Min, Sum
+from django.db.models.functions import Concat
 from store.models import Collection, Customer, OrderItem, Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(new_id=F("id"))
+    queryset = Customer.objects.annotate(
+        # CONCAT
+        full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
+    )
+
+    queryset = Customer.objects.annotate(
+        # CONCAT
+        full_name=Concat("first_name", Value(" "), "last_name")
+    )
 
     return render(request, "hello.html", {"name": "Devdhar", "result": list(queryset)})
