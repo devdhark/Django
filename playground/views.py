7ec7fb7b71131ea5c0f622089d9b30a23e18f2f2
devdhark <devdhar.kodiatar@gmail.com>
2023-08-17 16:27:28 -0500

    annotate

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
+from django.db.models import Q, F, Value
 from django.db.models.aggregates import Count, Max, Min, Sum
 from store.models import Collection, Customer, OrderItem, Product
 
 
 def say_hello(request):
-    result = Product.objects.aggregate(count=Count("id"), min_price=Min("unit_price"))
+    queryset = Customer.objects.annotate(new_id=F("id"))
 
-    return render(request, "hello.html", {"name": "Devdhar", "result": result})
+    return render(request, "hello.html", {"name": "Devdhar", "result": list(queryset)})
