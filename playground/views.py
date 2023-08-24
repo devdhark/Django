7a6525020c8b84285e1d0b11262b0840bf2ccac8
devdhark <devdhar.kodiatar@gmail.com>
2023-08-23 20:02:01 -0500

    expression wrapper

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Q, F, Value, Func, Count
+from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper, DecimalField
 from store.models import Customer, Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(orders_count=Count("order"))
+    discounted_price = ExpressionWrapper(
+        F("unit_price") * 0.8, output_field=DecimalField()
+    )
+
+    queryset = Product.objects.annotate(discounted_price=discounted_price)
 
     return render(request, "hello.html", {"name": "Devdhar", "result": list(queryset)})
