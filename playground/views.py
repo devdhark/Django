7753d0d72675dca4c4a269a85611c652f70fcbf3
devdhark <devdhar.kodiatar@gmail.com>
2023-08-23 19:49:28 -0500

    sync

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value, Func
-from django.db.models.aggregates import Count, Max, Min, Sum
-from django.db.models.functions import Concat
-from store.models import Collection, Customer, OrderItem, Product
+from django.db.models import Q, F, Value, Func, Count
+from store.models import Customer, Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
-    )
-
-    queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Concat("first_name", Value(" "), "last_name")
-    )
+    queryset = Customer.objects.annotate(orders_count=Count("order"))
 
     return render(request, "hello.html", {"name": "Devdhar", "result": list(queryset)})
