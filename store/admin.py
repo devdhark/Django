92c1b84827031c4b3f9211506a0a7837567dca21
devdhark <devdhar.kodiatar@gmail.com>
2023-08-28 21:20:38 -0500

    add items using inlines

--- store/admin.py
+++ store/admin.py
@@ -30,6 +30,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ["collection", "last_update", InventoryFilter]
     list_per_page = 10
     list_select_related = ["collection"]
+    search_fields = ["title"]
 
     def collection_title(self, product):
         return product.collection.title
@@ -50,9 +51,18 @@ class ProductAdmin(admin.ModelAdmin):
         )
 
 
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ["product"]
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ["customer"]
+    inlines = [OrderItemInline]
     list_display = ["id", "placed_at", "customer"]
 
 
